d9c997c77b0cb352fa818dc83db0280e1e2acd54
sepiranequep <42681742+sepiranequep@users.noreply.github.com>
2018-10-18 16:14:34 -0500

    Terminando las funciones de la actividad select

--- js/actividadSeleccionarModificar.js
+++ js/actividadSeleccionarModificar.js
@@ -1,1 +1,37 @@
-alert("asd");
+var respuestasCorrectas= ['select','nombre','mysqli_connect','mysqli_query','mysqli_connect','mysqli_query','select','mysqli_free_result','mysqli_free_result'];
+
+function revisarSeleccion(){   
+    var respuestasUser = document.getElementsByClassName('comparables');
+    var mensajeSelect = document.getElementById('mensajeSelect');
+    var contador = 0;
+    for (let i = 0; i < respuestasUser.length; i++) {
+        var minuscula = respuestasUser[i].value;
+        
+        if(minuscula.toLowerCase() === respuestasCorrectas[i]){            
+            respuestasUser[i].style.backgroundColor="#d4edda";
+            contador++;
+        }else{
+            respuestasUser[i].style.backgroundColor="#f8d7da";
+        }
+
+        if(contador === 9){
+            mensajeSelect.textContent="Todos las respuestas son correctas, ¡FELICITACIONES!";
+        }else{
+            mensajeSelect.textContent="";
+        }
+    }
+}
+function mostrarRespuestas(){
+    var respuestasUser = document.getElementsByClassName('comparables');
+    var mensajeSelect = document.getElementById('mensajeSelect');
+
+    for (let j = 0; j < respuestasUser.length; j++) {
+        mensajeSelect.textContent="";
+        respuestasUser[j].style.backgroundColor="#fff3cd";
+        respuestasUser[j].value = respuestasCorrectas[j];
+    }
+
+}
+function revisarModificar(){
+    
+}
